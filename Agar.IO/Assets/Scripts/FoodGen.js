3f28ba42117ce128a4b8bed246df887796f7f0e3
Mika <mika.boersma@vathorstcollege.nl>
2015-06-05 11:46:32 +0200

    Smooth camera, player, dot delay

--- Agar.IO/Assets/Scripts/FoodGen.js
+++ Agar.IO/Assets/Scripts/FoodGen.js
@@ -25,10 +25,11 @@ function Start () {
 		 	go.transform.parent = foodParent;
 			go.GetComponent.<Renderer>().material.color = Color.red;
 			go.transform.position = randomPosition;
+			go.transform.position.z = 2.0;
 			// Deze man doet heel erg geak. Ik snap niet waarom die hem samendrukt. Kiek ff of jou dat wel lukt.
 			go.transform.localRotation = Quaternion.AngleAxis(35.0, Vector2.up);
          	foodGameObject[i] = go;
-			
+			yield WaitForSeconds(0.5);
 		}
 		ShowHideIndicators(true);
 		
---
+++ Agar.IO/Assets/followCenter.js
@@ -0,0 +1,22 @@
+﻿ private var thisTransform : Transform;
+ private var velocity : Vector2;
+ 
+ var centerBlock : Transform;
+ var smoothTime = 0.3;
+ var xOffset : float = 1.0;
+ var yOffset : float = 1.0;
+  
+ 
+ 
+ function Start()
+ {
+        thisTransform = transform;
+ }
+ 
+ function LateUpdate()
+ {
+ 
+        thisTransform.position.x = Mathf.Lerp( thisTransform.position.x, centerBlock.position.x + xOffset, Time.deltaTime * smoothTime);
+ 
+        thisTransform.position.y = Mathf.Lerp( thisTransform.position.y, centerBlock.position.y + yOffset, Time.deltaTime * smoothTime);
+ }
