fae4ea47be16bc354a28dffa178cc97665ccf465
Mika <mika.boersma@vathorstcollege.nl>
2015-06-05 11:01:12 +0200

    Basic shit

---
+++ Agar.IO/Assets/NewBehaviourScript.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+private var mousePosition : Vector3;
+public var moveSpeed = 0.1f;
+public var Mass = 20.0f;
+
+function Start () {
+
+}
+
+function Update () {
+		
+		 mousePosition = Input.mousePosition;
+         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+         transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+   
+		}
